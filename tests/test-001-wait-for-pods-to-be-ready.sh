78db20d6cb14ab9502bf3f3846cd37643799e93b
Peter Grzybowski <peter@northern.tech>
2019-12-13 15:49:40 +0100

    MEN-2892 Mender Helm chart test modifications
    * modifications to the test:
      * waiting correctly for pods (in case they download image still)
      * max_wait added an timeout increased
      * jq used to verify that the token exists in JWT
    * misc logging and force removal of temp file
    
    ChangeLog:title
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- tests/test-001-wait-for-pods-to-be-ready.sh
+++ tests/test-001-wait-for-pods-to-be-ready.sh
@@ -5,8 +5,9 @@ set -e
 echo -n "> Waiting for all the PODs to become ready"
 
 n=0
-while [ $n -lt 60 ]; do
-    NOT_READY=$(kubectl get pods -o custom-columns=NAMESPACE:metadata.namespace,POD:metadata.name,READY-true:status.containerStatuses[*].ready | grep -v elasticsearch-master-0 | egrep -e 'false$' | wc -l)
+max_wait=512
+while [ $n -lt $max_wait ]; do
+    NOT_READY=$(kubectl get pods -o custom-columns=NAMESPACE:metadata.namespace,POD:metadata.name,READY-true:status.containerStatuses[*].ready | grep -v elasticsearch-master-0 | egrep -e 'false$' -e '<none>$' | wc -l)
     if [ $NOT_READY -eq 0 ]; then
         echo -e "\n> PODs area ready:"
         kubectl get pods -o custom-columns=NAMESPACE:metadata.namespace,POD:metadata.name,READY-true:status.containerStatuses[*].ready
