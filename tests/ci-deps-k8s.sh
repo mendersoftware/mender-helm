70744e2aa665401b8d2ebd2d168b6faa32e9dabc
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2020-02-06 14:07:51 +0100

    Upgrade elasticsearch to version 6, conductor to master
    Changelog: none
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- tests/ci-deps-k8s.sh
+++ tests/ci-deps-k8s.sh
@@ -6,8 +6,8 @@
 set -e
 
 log "deploying dependencies: ElasticSearch"
-kubectl apply -f service-elasticsearch5.yaml
-kubectl apply -f elasticsearch5.yaml
+kubectl apply -f service-elasticsearch.yaml
+kubectl apply -f elasticsearch.yaml
 
 log "installing helm"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
