f1664b73795f9e439d7fd97326ad64241863a99a
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2020-03-24 16:09:26 +0100

    Upgrade Mender chart to Mender 2.3.0
    Changelog: upgrade the chart to Mender 2.3.0
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- tests/variables.sh
+++ tests/variables.sh
@@ -4,5 +4,5 @@ export K8S_CLUSTER_NAME=mender-`date +%s`
 export K8S_VERSION=v1.12.10
 export MINIO_accessKey=AKIAIOSFODNN7EXAMPLE
 export MINIO_secretKey=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-export MENDER_HELM_CHART_VERSION=`cat ./mender/Chart.yaml | grep ^version: | awk '{print($NF);}'`
+export MENDER_HELM_CHART_VERSION=`cat ./mender/Chart.yaml | grep ^version: | awk '{print($NF);}' | sed -e 's/"//g'`
 
