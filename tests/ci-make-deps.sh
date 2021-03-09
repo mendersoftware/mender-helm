44c5c6491d8b4affdeaa7a00003cf4579ff91b6f
Lluis Campos <lluis.campos@northern.tech>
2021-03-09 15:11:54 +0100

    [tests/] Split ci-deps-k8s.sh into two scripts: install and deploy
    So that locally we can run the deploy one assuming all dependencies are
    in place.
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

---
+++ tests/ci-make-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. tests/variables.sh
+. tests/functions.sh
+
+set -e
+
+log "deploying dependencies: minio"
+helm install mender-minio minio/minio --version 6.0.5 --set accessKey=${MINIO_accessKey},secretKey=${MINIO_secretKey},persistence.enabled=false
+
+log "deploying dependencies: mongodb"
+helm install mender-mongo --set "auth.enabled=false,persistence.enabled=false" bitnami/mongodb
