06f3276ce1fd6a0eed9ea5557c4810bc644ee7eb
Lluis Campos <lluis.campos@northern.tech>
2021-03-11 13:06:26 +0100

    [ci-make-deps] Limit to minio memory request to 100Mi
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/ci-make-deps.sh
+++ tests/ci-make-deps.sh
@@ -6,7 +6,10 @@
 set -e
 
 log "deploying dependencies: minio"
-helm install mender-minio minio/minio --version 6.0.5 --set accessKey=${MINIO_accessKey},secretKey=${MINIO_secretKey},persistence.enabled=false
+helm install mender-minio minio/minio \
+    --version 6.0.5 \
+    --set resources.requests.memory=100Mi \
+    --set accessKey=${MINIO_accessKey},secretKey=${MINIO_secretKey},persistence.enabled=false
 
 log "deploying dependencies: mongodb"
 helm install mender-mongo --set "auth.enabled=false,persistence.enabled=false" bitnami/mongodb
