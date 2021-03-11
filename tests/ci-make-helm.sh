96c7500789c3f6ccb5c8894e26447d5c7064d297
Lluis Campos <lluis.campos@northern.tech>
2021-03-11 13:05:25 +0100

    [ci-make-helm] User friendly exit when no credentials in env
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- tests/ci-make-helm.sh
+++ tests/ci-make-helm.sh
@@ -6,6 +6,12 @@
 set -e
 
 log "installing Mender Helm chart mender-${MENDER_HELM_CHART_VERSION}.tgz"
+
+if [ -z "$REGISTRY_MENDER_IO_USERNAME" -o -z "$REGISTRY_MENDER_IO_PASSWORD" ]; then
+    log "error: missing registry.mender.io credentials"
+    exit 1
+fi
+
 helm install mender -f mender/values.yaml -f tests/keys.yaml -f tests/values.yaml --set global.image.username=${REGISTRY_MENDER_IO_USERNAME} --set global.image.password="${REGISTRY_MENDER_IO_PASSWORD}" --set global.s3.AWS_ACCESS_KEY_ID="${MINIO_accessKey}" --set global.s3.AWS_SECRET_ACCESS_KEY="${MINIO_secretKey}" mender-${MENDER_HELM_CHART_VERSION}.tgz || exit 3;
 
 log "pods:"
